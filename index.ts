3dfa3376e3bd01fe5566d10d387938887b56e13f
mzn <30986226+slayerinc@users.noreply.github.com>
2020-08-10 23:59:04 -0500

    Added command handler

--- index.ts
+++ index.ts
@@ -1,9 +1,31 @@
 // Imports
-import { Client, Message } from "discord.js";
+import { Client, Message, Collection } from "discord.js";
 import { prefix, token } from "./config.json";
+import { readdir } from "fs";
 
 // Definitions
 const client: Client = new Client();
+
+// Collections
+const commands = new Collection();
+
+// Command Handler
+readdir("./commands/", (err: NodeJS.ErrnoException, files: string[]) => {
+    if (err) {
+        console.error(err);
+    }
+
+    const jsfile = files.filter(file => file.split(".").includes("js"));
+
+    if (jsfile.length === 0) {
+        return console.error("No commands found");
+    }
+
+    jsfile.map(file => {
+        const pull = require(`./commands/${file}`);
+        commands.set(pull.name, pull);
+    });
+});
 
 // Ready event
 client.on("ready", () => {
@@ -13,7 +35,7 @@ client.on("ready", () => {
 // Message event
 client.on("message", async (message: Message) => {
     if (message.content == `${prefix}ping`) {
-        return message.channel.send(`:pong: Ping! O-O \`${client.ws.ping}\` :pong:`);
+        return message.channel.send(`:ping_pong: Ping! O-O \`${client.ws.ping}\` :ping_pong:`);
     }
 });
 
